2eff7b5c33eb6ce6d194b93398188576d227d055
Khanh Le <58643031+nkgotcode@users.noreply.github.com>
2022-10-05 21:59:07 +0700

    Initial Commit

---
+++ ArtistDatabase/ArtistDatabaseApp.swift
@@ -0,0 +1,17 @@
+//
+//  ArtistDatabaseApp.swift
+//  ArtistDatabase
+//
+//  Created by itsnk on 10/5/22.
+//
+
+import SwiftUI
+
+@main
+struct ArtistDatabaseApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
